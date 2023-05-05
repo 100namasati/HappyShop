3e1ec43b5bd746557e9c103739e7ff72b520c00f
chandrabhan@softopstechnologies.com <chandrabhan@softopstechnologies.com>
2023-05-05 14:04:22 +0530

    user navigation functionality done

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import logo from "./logo.svg";
 import Home from "./Component/Home";
 import "./App.css";
 import ProjectNavbar from "./Component/ProjectNavbar";
+import { Route, Routes } from "react-router-dom";
 
 function App(props: any) {
   return (
     <div className="App">
-      <Home {...props} />
+      <Routes>
+        <Route path="/" element={<Home {...props} />}  />
+      </Routes>
     </div>
   );
 }
--- src/Component/NavbarController.tsx
+++ src/Component/NavbarController.tsx
@@ -3,6 +3,8 @@ export interface states {
   drawer: boolean;
   drawerDirection: any;
   closeDrawer: boolean;
+  //user menu state
+  openMenu: boolean;
 }
 export type Props = {};
 export default class first extends React.Component<Props, states> {
@@ -12,6 +14,8 @@ export default class first extends React.Component<Props, states> {
       drawer: false,
       drawerDirection: "left",
       closeDrawer: false,
+      //user menu state
+      openMenu: false,
     };
   }
   openDrawer = () => {
@@ -21,4 +25,8 @@ export default class first extends React.Component<Props, states> {
   closeDrawer = () => {
     this.setState({ drawer: false });
   };
+  openUserMenu = () => {
+    this.setState({ openMenu: !this.state.openMenu });
+  };
+  
 }
