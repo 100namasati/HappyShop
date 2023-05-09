2eb7c09e59fcc2bd41a79ead9852a22f7045a38d
chandrabhan@softopstechnologies.com <chandrabhan@softopstechnologies.com>
2023-05-09 14:45:28 +0530

    auth routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import Home from "./Component/Home";
 import "./App.css";
-import ProjectNavbar from "./Component/ProjectNavbar";
+import ProjectNavbar from "./Component/Navbar/ProjectNavbar";
 import { Route, Routes } from "react-router-dom";
 import Register from "./Component/Register/Register";
 import Login from "./Component/Login/Login";
 import { withRouter } from "./HOC";
 import AccountLogin from "./Component/MyAccount/AccountLogin";
+import { Protected, AuthProtected } from "./Component/Protected/Protected";
+import Products from "./Component/Products/Products";
 function App(props: any) {
-  const [isUserAvailable, setIsUserAvailable] = useState(false);
-  useEffect(() => {
-    let user: any = localStorage.getItem("token");
-    if (user) {
-      setIsUserAvailable(true);
-    } else {
-      setIsUserAvailable(false);
-    }
-  }, []);
-
-  // console.log(this.state.isTokenAvailable);
-
   return (
     <div className="App">
-      <ProjectNavbar router={undefined} isUserAvailable={isUserAvailable} />
+      <ProjectNavbar router={undefined} />
       <Routes>
-        <Route path="/" element={<Home {...props} />} />
-        <Route path="/login" element={<Login {...props} />} />
-        <Route path="/register" element={<Register {...props} />} />
-        <Route path="/myaccount" element={<AccountLogin/>}/>
-        <Route path="*" element={<h1>404 page not found</h1>} />
+        <Route path="/" element={<Protected Component={Home} />} />
+        <Route path="/Products" element={<Protected Component={Products} />} />
+        <Route path="/login" element={<AuthProtected Component={Login} />} />
+        <Route path="/login" element={<AuthProtected Component={Register} />} />
+        s{" "}
       </Routes>
     </div>
   );
--- src/Component/Login/LoginController.tsx
+++ src/Component/Login/LoginController.tsx
@@ -29,9 +29,10 @@ export class LoginController extends Component<Props, States> {
   }
   handleLogin = () => {
     signInWithPopup(Auth, provider)
-      .then((result) => {
+      .then(async (result) => {
         const user = result.user;
-        console.log(user);
+        // console.log(user);
+        await this.props.router.navigate("/");
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -39,6 +40,7 @@ export class LoginController extends Component<Props, States> {
         const email = error.customData.email;
         const credential = GoogleAuthProvider.credentialFromError(error);
         alert(errorMessage);
+        this.props.router.navigate("/login");
       });
   };
   handleFacebookLogin = () => {
@@ -74,24 +76,25 @@ export class LoginController extends Component<Props, States> {
   onFinish = (values: any) => {
     // console.log("Success:", values);
     this.setState({ email: values.email, password: values.password }, () => {
-      // console.log("Success:", this.state.email);
-      // console.log("Success:", this.state.password);
+      console.log("Success:", this.state.email);
+      console.log("Success:", this.state.password);
       signInWithEmailAndPassword(Auth, this.state.email, this.state.password)
-        .then((res) => {
+        .then(async (res) => {
           // console.log("@@@@_______", res.user.uid);
           let user = localStorage.getItem("token");
           if (user) {
-            localStorage.clear();
-            localStorage.setItem("token", JSON.stringify(res.user.uid));
-            // this.forceUpdate();
-            this.props.router.navigate("/");
-            // this.props.router.navigate("/", {
-            //   state: { data: res.user.uid },
-            // });
+            await localStorage.clear();
+            await localStorage.setItem("token", JSON.stringify(res.user.uid));
+            await this.forceUpdate();
+            console.log("logged in");
+
+            await this.props.router.navigate("/products");
           } else {
-            localStorage.setItem("token", JSON.stringify(res.user.uid));
-            this.forceUpdate();
-            this.props.router.navigate("/");
+            await localStorage.setItem("token", JSON.stringify(res.user.uid));
+            // this.forceUpdate();
+            console.log("logged in");
+
+            await this.props.router.navigate("/products");
           }
 
           // console.log(user);
@@ -99,9 +102,7 @@ export class LoginController extends Component<Props, States> {
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
-          if (errorMessage) {
-            alert("You have wrong crendital");
-          }
+          console.log(error.message);
         });
     });
   };
