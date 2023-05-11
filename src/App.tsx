990d673c3a33900caa53925e644fcc3dbc5e146a
chandrabhan@softopstechnologies.com <chandrabhan@softopstechnologies.com>
2023-05-11 18:16:58 +0530

    account route added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Home from "./Components/Home/Home";
 import "./App.css";
-<<<<<<< HEAD
 import ProjectNavbar from "./Components/Navbar/ProjectNavbar";
 import { Route, Routes } from "react-router-dom";
 import Register from "./Components/Register/Register";
 import Login from "./Components/Login/Login";
 import { withRouter } from "./HOC";
-=======
-import ProjectNavbar from "./Component/ProjectNavbar";
-import { Route, Routes } from "react-router-dom";
->>>>>>> origin
 
 import { Protected, AuthProtected } from "./Components/Protected/Protected";
 import Products from "./Components/Products/Products";
@@ -19,7 +14,6 @@ import AccountLogin from "./Components/MyAccount/AccountLogin";
 function App(props: any) {
   return (
     <div className="App">
-<<<<<<< HEAD
       <ProjectNavbar router={undefined} />
       <Routes>
         <Route path="/" element={<Protected Component={Home} />} />
@@ -30,10 +24,6 @@ function App(props: any) {
         <Route path="/login" element={<AuthProtected Component={Login} />} />
 
         <Route path="/register" element={<Register />} />
-=======
-      <Routes>
-        <Route path="/" element={<Home {...props} />}  />
->>>>>>> origin
       </Routes>
     </div>
   );
